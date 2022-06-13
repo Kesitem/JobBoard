c3d1b01906dd012a50920d9dfc484f2c02dbb8b0
Kesitem <kesitem@gmail.com>
2022-06-13 17:23:54 +0200

    Use static file

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
 
-def include_router(app):
-    app.include_router(general_pages_router)
+def include_router(application):
+    application.include_router(general_pages_router)
+
+
+def configure_static(application):
+    application.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
-    include_router(app)
-    return app
+    application = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(application)
+    configure_static(application)
+    return application
 
 
 app = start_application()
 
-
-# @app.get("/")
-# def hello_api():
-#     return {"msg": "Hello API"}
