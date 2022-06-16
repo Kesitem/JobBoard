762af5ec8a777901a133c7ac083fb064af3d13c9
Kesitem <kesitem@gmail.com>
2022-06-16 21:55:07 +0200

    Connect to database

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine
+from db.base_class import Base
 
 
 def include_router(application):
@@ -12,12 +14,16 @@ def configure_static(application):
     application.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
 def start_application():
     application = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(application)
     configure_static(application)
+    create_tables()
     return application
 
 
 app = start_application()
-
