c6261f53a7665f56ccbb18af7eba6f154d3ded0f
Kesitem <kesitem@gmail.com>
2022-06-18 19:07:05 +0200

    Add JWT token Generator

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,19 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 settings = Settings()
