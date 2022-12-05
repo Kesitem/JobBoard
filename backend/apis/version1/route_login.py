60d8dd37a002099aa93ed58ba36031f6f46232d7
Kesitem <kesitem@gmail.com>
2022-12-05 21:31:52 +0100

    Put authentication token in HttpOnly cookie

--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -9,19 +9,21 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
-from fastapi.security import OAuth2PasswordBearer
+# from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import OAuth2PasswordRequestForm
 from jose import jwt
 from jose import JWTError
 from schemas.tokens import Token
 from sqlalchemy.orm import Session
 
+from fastapi import Response
+from apis.utils import OAuth2PasswordBearerWithCookie
+
 router = APIRouter()
 
 
 def authenticate_user(username: str, password: str, db: Session):
     user = get_user(username=username, db=db)
-    print(user)
     if not user:
         return False
     if not Hasher.verify_password(password, user.hashed_password):
@@ -30,9 +32,9 @@ def authenticate_user(username: str, password: str, db: Session):
 
 
 @router.post("/token", response_model=Token)
-def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
-):
+def login_for_access_token(response: Response,
+                           form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+                           ):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
@@ -43,15 +45,15 @@ def login_for_access_token(
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
-
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
 
 
 def get_current_user_from_token(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+        token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
