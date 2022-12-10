e2fc0fc5d4f95ec3877c0586fa715ebb74e3d182
Kesitem <kesitem@gmail.com>
2022-12-10 09:50:00 +0100

    Add login in webapps

--- backend/webapps/base.py
+++ backend/webapps/base.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from webapps.jobs import route_jobs
 from webapps.users import  route_users
-
+from webapps.auth import route_login
 
 api_router = APIRouter()
 api_router.include_router(route_jobs.router, prefix="", tags=["job-webapp"])
 api_router.include_router(route_users.router, prefix="", tags=["users-webapps"])
+api_router.include_router(route_login.router, prefix="", tags=["auth-webapp"])
